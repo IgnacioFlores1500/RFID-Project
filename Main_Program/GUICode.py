7188f303c035f21aab9f3c7f7687287014bb983f
Omgiszbeast <73628679+Omgiszbeast@users.noreply.github.com>
2021-11-12 03:12:44 -0600

    pt2

--- Main_Program/GUICode.py
+++ Main_Program/GUICode.py
@@ -14,10 +14,12 @@ from tkinter import ttk
 from tkinter import messagebox
 # sublibrary to ask for any info the program needs from the user 
 from tkinter import simpledialog
+from tkinter.constants import END
 # sublibrary to 
 
-#Imports a function from database file to return if a personID has edit_prilvages
+##Imports a function from database file to return if a personID has edit_prilvages
 from dataBaseCode import returnsAdminStatusViaPersonID
+from dataBaseCode import returnsInfomationFromPeopleTableFromFirstName
 
 TrueUser = False
 TruePassword = False
@@ -45,18 +47,34 @@ class RFIDDisplay:
     #isAdmin sends what the user input for logging in. This will be the log in page for the UI and
     #if the username and password are correct then it will provide access to the classroom state and 
     #modifying priveliges to the classroom as well
-    def isAdminFromPersonID(self, personID):
+    def isAdminFromFirstName(self, event, firstName):
         #def isAdmin(self, event, accounts):
         #taking data entered into the entry and chcecking it for authentification
-        #enteredName = self.my_entry.get()
+        
+        print("hello")
+        ##idk what this actually does -iggy
+        enteredName = self.my_entry.get()
+        print(enteredName)
+        ##Grabs tuple of people data based from the user 
+        resultingInfo = returnsInfomationFromPeopleTableFromFirstName(firstName)
+
+        ## 
+        if resultingInfo:
+            grabbedPersonID = resultingInfo[0]
+        else:
+            return False
+
         
 
-        ##Calls returnsFunction from databaseCode
-        Status = returnsAdminStatusViaPersonID(personID)
+        ##Using the KeyID from the people table, we look returnsAdmin...Function 
+        ##that looks at the EditPrivages
+        Status = returnsAdminStatusViaPersonID(grabbedPersonID) ##This should be either a 1 or 0
 
         ##Test that Status
         if (Status == 1):
             print("TRUETEST")
+            TrueUser = True
+            TruePassword = True
             return True
         else:
             print("FALSETEST")
@@ -84,21 +102,55 @@ class RFIDDisplay:
         
         #a variable that is present to check if the username is registered in the data base
         
-        if passwd == "Hello":
-            TruePassword = True
-        else:
-            TruePassword = False
+
+#####################################################################################
+        ##Old Method of True Admin: Leave commented for now
+        ##First Section: GoodBye
+        ##Second Section: Hello
+        ## if passwd == "Hello":
+        ##     TruePassword = True
+        ## else:
+        ##     TruePassword = False
             
-        if user == "GoodBye":
-            TrueUser = True
+        ## if user == "GoodBye":
+        ##     TrueUser = True
+        ## else:
+        ##     TrueUser = False
+#####################################################################################
+
+        ##Leave Password true atm
+        TruePassword = True
+
+        firstNameDBFinal = ()
+        ##This returns a tuple of the people table from the firstName you put in user = self.name_entry.get()
+        firstNameFromDatasBase = returnsInfomationFromPeopleTableFromFirstName(user)
+        ##lastNameFromDataBase =
+
+        ##If the tuple does exist, (aka there is someone called whatever the user puts)
+        ##Grab the firstname of the tuple
+
+        if firstNameFromDatasBase:
+            firstNameDBFinal = firstNameFromDatasBase[1]
+            ##Kinda not needed, but it's like a second check
+            ##Checsk to see the resulted firstName is the same as the user inputed
+            if (firstNameDBFinal == user):
+                    TrueUser = True
+            else:
+                TrueUser = False
+           
+        ## if there is no tuple of the user, the person doesn't exist in the database
         else:
             TrueUser = False
-        
+
+            
+            
+#####################################################################################       
         if TrueUser == False or TruePassword == False:
             self.display = ttk.Label(self.window, text="Invalid Login LOL")
             self.display.grid(row=4, column=0, sticky=tk.W, pady=5)
             CurrentFailedAttempt = True
         else:
+            #If Above passes, show the create classroom -iggy
             self.show_admin_powers()
             self.login_button.destroy()
             
@@ -114,9 +166,11 @@ class RFIDDisplay:
     #Create class is meant to make the classroom on the UI by making empty seats.
     def create_class(self, event):
         classSize = self.classroom.get()
+        ###I assume to type_cast classSize from string to integer
         classSize = int(classSize)
         global seats
         global recentClassSize
+        hasBeenCreated = True
         if recentClassSize != 0:
             for i in range(recentClassSize):
                 for j in range(2, recentClassSize + 2):
@@ -149,7 +203,7 @@ class RFIDDisplay:
         self.name_entry.grid(row=1, column=0, sticky=tk.W, pady=5)
         #self.name_entry.insert(tk.END, "Enter User Name")
         self.name_entry.insert(tk.END, "GoodBye")
-        self.name_entry.bind("<Return>", self.isAdmin)
+        self.name_entry.bind("<Return>", self.validLogin)
         
         #setting up when the user can enter a password
         self.password_entry = ttk.Entry(self.window, width=100)
@@ -169,10 +223,22 @@ class RFIDDisplay:
             self.classroom.insert(tk.END, "Enter the class size you want (Perfect Square)")
             self.classroom.bind("<Return>", self.create_class)
             RecentClassSize = self.classroom.get()
+
+        ##Random some function that might not bloodywork?
+        if (hasBeenCreated == True) and (TrueUser == False or TruePassword == False):
+            self.classroom.delete(ttk.END, END)
             
         #creating a label for most recent log in
         self.recentTag = ttk.Label(self.window, text="This is the most Recent login")
-        self.recentTag.grid(row=5, column=0,sticky=tk.W, pady=20)
+        self.recentTag.grid(row=6, column=0,sticky=tk.W, pady=20)
+
+        
+        ##Update button, I just wanted a GUI way approach to adding People to the Database
+        ## WIP: SHIT DOESN"TT WORK DOES NOT WORK - IGGY
+        if TrueUser == True and TruePassword == True:
+            self.insert_Record_Button = tk.Button(self.window, text="Insert Users")
+            ##Plac on the grid
+            self.insert_Record_Button.grid(row=10, column=10, sticky=tk.W, pady=5)
         
         #The Following code is for the display of seats. The seats are represented by brackets and can be empty
         
